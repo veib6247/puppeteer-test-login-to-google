0a37bac9ae86dfdcc5d19900a0c2e5d89b2d7e90
Bryan Olandres <ozoneblacklight@outlook.com>
2023-06-12 12:48:41 +0800

    added env and final screenshot to check

--- puppet.js
+++ puppet.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer'
+import 'dotenv/config'
 
 /**
  *
@@ -6,17 +7,47 @@ import puppeteer from 'puppeteer'
 const imARealBoi = async () => {
   // launch own chrome
   const browser = await puppeteer.launch({
-    headless: 'new',
+    headless: 'false',
   })
 
   const page = await browser.newPage()
+  await page.goto('https://accounts.google.com')
+  await page.setViewport({ width: 1080, height: 1024 })
 
-  await page.goto('https://kobeni-nuxt.vercel.app/login')
+  try {
+    /**
+     * EMAIL
+     */
+    console.log('Locating email input...')
+    const inputEmail = await page.waitForSelector('#identifierId')
 
-  // Query for an element handle.
-  const loginButton = await page.waitForSelector('.btn')
+    console.log('Typing in email and pressing Enter...')
+    await inputEmail.type(process.env.GOOGLE_EMAIL)
+    await inputEmail.press('Enter')
 
-  console.log(await loginButton.click())
+    await page.waitForNetworkIdle()
+
+    /**
+     * PASSWORD
+     */
+    console.log('Locating password input...')
+    const inputPassword = await page.waitForSelector('input[type="password"]')
+
+    console.log('Typing in password and pressing Enter...')
+    await inputPassword.type(process.env.GOOGLE_PASSWORD)
+    await inputPassword.press('Enter')
+
+    await page.waitForNetworkIdle()
+    console.log(page.url())
+    //
+  } catch (error) {
+    console.error(error)
+
+    //
+  } finally {
+    await page.screenshot({ path: 'screenshots/final.png' })
+    await browser.close()
+  }
 }
 
 // run da boi!!!
